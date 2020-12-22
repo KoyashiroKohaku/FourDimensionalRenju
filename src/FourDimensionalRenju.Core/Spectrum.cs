02583435621c4c06653874ff9a00cc5222fa11e2
Koyashiro Kohaku <develop@koyashiro.net>
2020-12-23 01:32:51 +0900

    feat: add `Spectrum.cs`

---
+++ src/FourDimensionalRenju.Core/Spectrum.cs
@@ -0,0 +1,43 @@
+namespace FourDimensionalRenju
+{
+    /// <summary>
+    /// 波長
+    /// </summary>
+    public enum Spectrum
+    {
+        /// <summary>
+        /// 赤
+        /// </summary>
+        Red = 0x00000000,
+
+        /// <summary>
+        /// 橙
+        /// </summary>
+        Orange = 0x00000001,
+
+        /// <summary>
+        /// 黄
+        /// </summary>
+        Yellow = 0x00000002,
+
+        /// <summary>
+        /// 緑
+        /// </summary>
+        Green = 0x00000003,
+
+        /// <summary>
+        /// 青
+        /// </summary>
+        Blue = 0x00000004,
+
+        /// <summary>
+        /// 藍
+        /// </summary>
+        Indigo = 0x00000005,
+
+        /// <summary>
+        /// 紫
+        /// </summary>
+        Violet = 0x00000006,
+    }
+}
